aa7821e815c6673c5fe99a80619e58170f990a35
Mikhail Kopylov <saram139@mail.ru>
2024-12-31 17:02:06 +0300

    1.0

---
+++ module_1_2.py
@@ -0,0 +1,7 @@
+completed_homework = 12
+spent_hours = 1.5
+course_name = "Python"
+time_for_one_task = spent_hours / completed_homework
+print(
+    f"Курс: {course_name}, всего задач: {completed_homework}, затрачено часов: {spent_hours}, среднее время выполнения {time_for_one_task} часа."
+)
